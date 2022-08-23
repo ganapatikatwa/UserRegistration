5912fcf8436baedfadf6ac30dd4a4ab8c2d43571
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-23 18:22:30 +0530

    [Ganapati]Last Name

--- RegexDemo/Program.cs
+++ RegexDemo/Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using RegexDemo;
+
 
 class Program
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Enter Name");
-        var data = Console.ReadLine();
-        String namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
-        Regex regex = new Regex(namepattern);
-        if (regex.IsMatch(data))
-        {
-            Console.WriteLine("Name is Valid");
-        }
-        else
-        {
-            Console.WriteLine("Name is not Valid");
-        }
+        UserRegex user = new UserRegex();
+        user.LastName();
+        
     }
+
+    
 }
---
+++ RegexDemo/UserRegex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class UserRegex
+
+    { public void LastName()
+        {
+            Console.WriteLine("Last Name");
+            var data = Console.ReadLine();
+            String namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(data))
+            {
+                Console.WriteLine("Last is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Last is not Valid");
+            }
+        }
+    }
+}
