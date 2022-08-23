ba2afa0915562a5ed504d46928559457b97124ac
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-23 17:54:34 +0530

    [Ganapati]First Name

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter Name");
+        var data = Console.ReadLine();
+        String namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        Regex regex = new Regex(namepattern);
+        if (regex.IsMatch(data))
+        {
+            Console.WriteLine("Name is Valid");
+        }
+        else
+        {
+            Console.WriteLine("Name is not Valid");
+        }
+    }
+}
