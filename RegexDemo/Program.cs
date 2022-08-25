76d43728d49df6a31e17a8f170fbebc1d5e7c2a9
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-25 12:48:38 +0530

    [Ganapati]Using one Upper Case and one Numeric

--- RegexDemo/Program.cs
+++ RegexDemo/Program.cs
@@ -3,14 +3,34 @@ using System.Text.RegularExpressions;
 using RegexDemo;
 
 
-class Program
+public class Program
 {
     static void Main(String[] args)
     {
         UserRegex user = new UserRegex();
-        user.Password();
-        
-    }
 
-    
+        bool n = true;
+        while (n)
+        {
+            Console.WriteLine("Choose option 1. Firstname 2.Last Name 3.Email 4. mobile no 5.password" +
+                "\n6.password With One upper case 7.password With One upper case and oneNumeric");
+            int option = Int32.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1: user.Name(); break;
+                case 2: user.LastName(); break;
+                case 3: user.EMail(); break;
+                case 4: user.PhoneNumber(); break;
+                case 5: user.Password(); break;
+                case 6: user.PasswordUpperCase(); break;
+                case 7: user.PasswordUpperCaseNumber(); break;
+                default:
+                    n = false;
+                    Console.WriteLine("Enter valid option"); break;
+            }
+        }
+        
+
+
+    }
 }
--- RegexDemo/UserRegex.cs
+++ RegexDemo/UserRegex.cs
@@ -9,11 +9,109 @@ namespace RegexDemo
 {
     public class UserRegex
 
-    { public void Password()
+    {
+        public void Name()
+        {
+        Console.WriteLine("Enter Name");
+        var data = Console.ReadLine();
+        String namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        Regex regex = new Regex(namepattern);
+        if (regex.IsMatch(data))
+        {
+            Console.WriteLine("Name is Valid");
+        }
+        else
+        {
+            Console.WriteLine("Name is not Valid");
+        }
+    }
+        public void LastName()
+        {
+            Console.WriteLine("Last Name");
+            var data = Console.ReadLine();
+            String namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(data))
+            {
+                Console.WriteLine("Last is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Last is not Valid");
+            }
+        }
+
+        public void EMail()
+        {
+            Console.WriteLine("e mail");
+            var data = Console.ReadLine();
+            String namepattern = "^[a-z]{3}[.]{1}([a-z]{3}){0,1}[@]{1}[a-z]{2}[.]{1}[a-z]{2,3}[.]{1}([a-z]{2}){0,1}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(data))
+            {
+                Console.WriteLine("e mail is Valid");
+            }
+            else
+            {
+                Console.WriteLine("e mail is not Valid");
+            }
+        }
+
+        public void PhoneNumber()
+        {
+            Console.WriteLine("Phone Number");
+            var data = Console.ReadLine();
+            String namepattern = "^[0-9]{2}[ ]{1}[6-9]{1}[0-9]{9}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(data))
+            {
+                Console.WriteLine("Phone number is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Phone number is not Valid");
+            }
+        }
+
+        public void Password()
+        {
+            Console.WriteLine("Password");
+            var data = Console.ReadLine();
+            String namepattern = "^[a-zA-Z]{8,}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(data))
+            {
+                Console.WriteLine("Password is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Password is not Valid");
+            }
+        }
+
+
+
+        public void PasswordUpperCase()
         {
             Console.WriteLine("Password Minimum 1 Upper Case");
             var data = Console.ReadLine();
             String namepattern = "^(?=.*?[A-Z])[a-zA-Z]{8,}$";
+            Regex regex = new Regex(namepattern);
+            if (regex.IsMatch(data))
+            {
+                Console.WriteLine("Password is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Password is not Valid");
+            }
+        }
+
+        public void PasswordUpperCaseNumber()
+        {
+            Console.WriteLine("Password Minimum 1 Upper Case 1 Numeric Case");
+            var data = Console.ReadLine();
+            String namepattern = "^(?=.*?\[d])(?=.*?[A-Z])[a-zA-Z]{8,}$";
             Regex regex = new Regex(namepattern);
             if (regex.IsMatch(data))
             {
